6621f2ebbc9edaf220407eb8fb64f8fc0f33560f
sanavanand <46275143+sanavanand@users.noreply.github.com>
2022-02-09 15:49:20 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,4 +19,4 @@ fi
 cf api ${INPUT_API} ${cf_opts}
 CF_USERNAME=${INPUT_USERNAME} CF_PASSWORD=${INPUT_PASSWORD} cf auth
 cf target -o ${INPUT_ORG} -s ${INPUT_SPACE}
-cf push -f ${INPUT_MANIFEST} ${INPUT_COMMAND}
+cf push -f .\${INPUT_MANIFEST} ${INPUT_COMMAND}
